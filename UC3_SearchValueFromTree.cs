f073b2f5c02b142f14d6e5ad6bab10a7b7d069d1
zeenat08 <89180148+zeenat08@users.noreply.github.com>
2021-10-04 23:58:30 +0530

    ZEENAT :[Added] Created a Binary Search Tree, Added Values And Searching A Value From The Tree

---
+++ UC3_SearchValueFromTree.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day15_BinarySearchTree_Assignment
+{
+     class UC3_SearchValueFromTree<T> where T : IComparable<T>
+     {
+            public T NodeData { get; set; }
+            public UC3_SearchValueFromTree<T> leftTree { get; set; }
+            public UC3_SearchValueFromTree<T> rightTree { get; set; }
+            public UC3_SearchValueFromTree(T nodeData)
+            {
+                this.NodeData = nodeData;
+                this.rightTree = null;
+                this.leftTree = null;
+            }
+
+            int leftCount = 0, rightCount = 0;
+            bool result = false;
+
+            public void Insert(T item)
+            {
+                T currentNodeValue = this.NodeData;
+                if ((currentNodeValue.CompareTo(item)) > 0)
+                {
+                    if (this.leftTree == null)
+                        this.leftTree = new UC3_SearchValueFromTree<T>(item);
+
+                    else
+                        this.leftTree.Insert(item);
+                    Console.WriteLine(item + " added to left");
+                }
+                else
+                {
+                    if (this.rightTree == null)
+                        this.rightTree = new UC3_SearchValueFromTree<T>(item);
+
+                    else
+                        this.rightTree.Insert(item);
+                    Console.WriteLine(item + " added to right");
+                }
+            }
+
+            public bool Search(T element, UC3_SearchValueFromTree<T> node)
+            {
+                if (node == null)
+                    return false;
+
+                if (node.NodeData.Equals(element))
+                {
+                    Console.WriteLine("Found the element in BST" + " " + node.NodeData);
+                    result = true;
+                }
+                else
+                    Console.WriteLine("Current element is {0} in BST", node.NodeData);
+
+                if (element.CompareTo(node.NodeData) < 0)
+                    Search(element, node.leftTree);
+
+                if (element.CompareTo(node.NodeData) > 0)
+                    Search(element, node.rightTree);
+
+                return result;
+            }
+
+            public void Display()
+            {
+                if (this.leftTree != null)
+                {
+                    this.leftCount++;
+                    this.leftTree.Display();
+                }
+                Console.WriteLine(this.NodeData.ToString());
+
+                if (this.rightTree != null)
+                {
+                    this.rightCount++;
+                    this.rightTree.Display();
+                }
+            }
+     }
+}
+
